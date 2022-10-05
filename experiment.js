535aeaf07119913332f550f446f5df04342b5fc0
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2022-10-05 08:05:25 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:experiment.js

--- experiment.js
+++ experiment.js
@@ -1,11 +1,20 @@
 async function main () {
-  return await makePromise()
+  return makePromise()
 }
+
+new Promise((resolve, reject ) => {
+  resolve()
+}).then(() => {
+  return new Promise((resove, reject ) => {
+    throw 
+    resolve('something')
+  })
+}) 
 
 async function makePromise () {
   await Promise.resolve()
-  // throw new Error('BEEP BEEP')
-  return Promise.reject(new Error('some error'))
+  throw new Error('BEEP BEEP')
+  // return Promise.reject(new Error('some error'))
 }
 
 main().catch(error => console.log(error.stack))
