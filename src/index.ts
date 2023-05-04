543997c04ea991cf196ae6cd1e00118ba75e632b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-05-04 13:39:16 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -41,51 +41,18 @@ function fromEntries<T> (iterable: Array<[string, T]>): Record<string, T> {
     return obj
   }, {})
 }
-export const rules = {
 
-}
-const config: Linter.Config = {
-  extends: 'eslint-config-standard',
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  rules,
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        'constructor-super': 'off', // ts(2335) & ts(2377)
-        'getter-return': 'off', // ts(2378)
-        'no-const-assign': 'off', // ts(2588)
-        'no-dupe-args': 'off', // ts(2300)
-        'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
-        'no-dupe-keys': 'off', // ts(1117)
-        'no-func-assign': 'off', // ts(2539)
-        'no-import-assign': 'off', // ts(2539) & ts(2540)
-        'no-new-symbol': 'off', // ts(2588)
-        'no-obj-calls': 'off', // ts(2349)
-        'no-redeclare': 'off', // ts(2451)
-        'no-setter-return': 'off', // ts(2408)
-        'no-this-before-super': 'off', // ts(2376)
-        'no-undef': 'off', // ts(2304)
-        'no-unreachable': 'off', // ts(7027)
-        'no-unsafe-negation': 'off', // ts(2365) & ts(2360) & ts(2358)
-        'no-var': 'error', // ts transpiles let/const to var, so no need for vars any more
-        'prefer-const': 'error', // ts provides better types with const
-        'prefer-rest-params': 'error', // ts provides better types with rest args over arguments
-        'prefer-spread': 'error', // ts transpiles spread to apply, so no need for manual apply
-        'valid-typeof': 'off', // ts(2367)
-      }
-    }
-      rules: {
+export const rules = {
         'comma-dangle': 'off',
 
         // TypeScript has this functionality by default:
         'no-undef': 'off',
 
-        // Rules replaced by @typescript-eslint versions:
-        ...fromEntries(equivalents.map((name) => [name, 'off'])),
         camelcase: 'off',
         'no-use-before-define': 'off',
+
+        // Rules replaced by @typescript-eslint versions:
+        ...fromEntries(equivalents.map((name) => [name, 'off'])),
 
         // @typescript-eslint versions of Standard.js rules:
         ...fromEntries(equivalents.map((name) => [`@typescript-eslint/${name}`, ruleFromStandard(name)])),
@@ -251,6 +218,14 @@ const config: Linter.Config = {
         '@typescript-eslint/type-annotation-spacing': 'error',
         'no-void': ['error', { allowAsStatement: true }]
       }
+const config: Linter.Config = {
+  extends: 'eslint-config-standard',
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
     }
   ]
 }
